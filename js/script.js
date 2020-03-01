00dfc1a687b1997b9ad1850301e06dd6ec18df24
Septiana Yoga <septianayoga30@gmail.com>
2020-03-02 00:13:13 +0700

    add button listener for start

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-// script will goes here
+const startScreenContainer = document.querySelector('.start-screen-container') // getting start screen container
+const inputPlayerScreen = document.querySelector('.input-player-screen') // getting input player screen
+
+const startButton = document.querySelector('.start-button') // getting start button
+const playButton = document.querySelector('.play-button') // getting the play button
+
+const playerOneName = document.querySelector('.player-one-input') // getting player 1 name
+const playerTwoName = document.querySelector('.player-two-input') // getting player 2 name
+
+function buttonStart() {
+    startScreenContainer.style.display = "none"
+    inputPlayerScreen.style.display = "block"
+}
+
+function buttonPlay() {
+    console.log(playerOneName.value)
+    console.log(playerTwoName.value)
+}
+
+startButton.addEventListener('click', buttonStart) // start button
+playButton.addEventListener('click', buttonPlay) // play button
