1885180cb071bcec6a104b36514a0812516515cc
Septiana Yoga <septianayoga30@gmail.com>
2020-03-03 14:46:09 +0700

    add system for entering the game

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const startScreenContainer = document.querySelector('.start-screen-container') // getting start screen container
 const inputPlayerScreen = document.querySelector('.input-player-screen') // getting input player screen
+const gameScreen = document.querySelector('.game-screen')
 
 const startButton = document.querySelector('.start-button') // getting start button
 const playButton = document.querySelector('.play-button') // getting the play button
@@ -7,14 +8,26 @@ const playButton = document.querySelector('.play-button') // getting the play bu
 const playerOneName = document.querySelector('.player-one-input') // getting player 1 name
 const playerTwoName = document.querySelector('.player-two-input') // getting player 2 name
 
+// for show the name of the player
+const playerOneScreen = document.getElementById('player-one-screen')
+const playerTwoScreen = document.getElementById('player-two-screen')
+
 function buttonStart() {
     startScreenContainer.style.display = "none"
     inputPlayerScreen.style.display = "block"
 }
 
 function buttonPlay() {
-    console.log(playerOneName.value)
-    console.log(playerTwoName.value)
+    // check if the player is entering the name
+    if(!playerOneName.value || !playerTwoName.value) {
+        alert('Please enter name for player!')
+    } else {
+        inputPlayerScreen.style.display = 'none'
+        gameScreen.style.display = 'block'
+        playerOneScreen.innerHTML = playerOneName.value
+        playerTwoScreen.innerHTML = playerTwoName.value
+    }
+
 }
 
 startButton.addEventListener('click', buttonStart) // start button
