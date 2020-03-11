d26b3fbdb85130af35585b808c8f0c38bbc9e128
Septiana Yoga <septianayoga30@gmail.com>
2020-03-11 11:55:56 +0700

    add initial version with oop

---
+++ js/scriptOopVersion.js
@@ -0,0 +1,14 @@
+// constructor for start game
+function StartGame() {
+
+}
+
+// constructor for player object
+function Player() {
+
+}
+
+// constructor for game play
+function GamePlay() {
+  
+}
